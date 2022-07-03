886dbbf92d07b866b40f9575f4952c96b72f9102
victorl2 <victorock20@gmail.com>
2022-07-03 16:06:49 -0300

    removing target folder with compiled binary

--- target/debug/deps/trade_optimizer-b142205543968541.d
+++
@@ -1,6 +0,0 @@
-/home/victor/code/trade_optimizer/target/debug/deps/trade_optimizer-b142205543968541.rmeta: src/main.rs src/candlestick.rs
-
-/home/victor/code/trade_optimizer/target/debug/deps/trade_optimizer-b142205543968541.d: src/main.rs src/candlestick.rs
-
-src/main.rs:
-src/candlestick.rs:
